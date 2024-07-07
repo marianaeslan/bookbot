7eece2565df60249a30a62950d13e778984e610f
marianaeslan <mariana.eslan@gmail.com>
2024-07-07 14:11:23 -0400

    function to sort the dictionary and print a report

--- main.py
+++ main.py
@@ -22,17 +22,35 @@ def count_char(book_name):
     # converting to lower case to avoid duplicates
     lower_case = file_contents.lower()
     characters = {}
+    # counting how many time each char appears on the book
     for char in lower_case:
-        if char in characters:
-            characters[char] +=1
-        else: 
-            characters[char] = 1
+        if char.isalpha():
+            if char in characters:
+                characters[char] +=1
+            else: 
+                characters[char] = 1
     return characters
 
-main("frankenstein.txt")
+def sort_dict(dictionary):
+    char_list = [{'char': k, 'count': v} for k,v in dictionary.items()]
+    return sorted(char_list, key=lambda x: x['count'], reverse = True)
 
-total_words = count_words("frankenstein.txt")
-print("Total words: ", total_words)
+book_name = "frankenstein.txt"
+main(book_name)
+print("\n")
 
-character_count = count_char("frankenstein.txt")
-print("Character counts: ", character_count)
+print (f"--- Begin report of books/{book_name} ---")
+
+total_words = count_words(book_name)
+print(f"{total_words} words found in the document")
+print("\n")
+
+character_count = count_char(book_name)
+sorted_characters = sort_dict(character_count)
+
+for item in sorted_characters:
+    print (f"The '{item['char']}' character was found {item['count']} times")
+
+print("\n")
+print("--- End report ---")
+
