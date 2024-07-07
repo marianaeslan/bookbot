a6a4264129a6a0c3d4bcd9d5142f47a9ca946118
marianaeslan <mariana.eslan@gmail.com>
2024-07-07 11:11:55 -0400

    function to read a book and print

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def read_book(book_name):
     # open a file and read it
     with open("books/" + book_name) as f:
         return f.read()
+        
+def main(book_name):
+    # read book and print it
+    file_contents = read_book(book_name)
+    print(file_contents)
+
